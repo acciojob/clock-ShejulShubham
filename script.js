77ff23d1dfe2403902be763664209d320d195d11
ShejulShubham <148786777+ShejulShubham@users.noreply.github.com>
2025-11-21 10:52:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const timer = document.getElementById("timer");
+
+setInterval(()=>{
+	const date = new Date();
+	const dateString = date.toLocaleDateString();
+	const time = date.toLocaleTimeString();
+
+	timer.textContent = `${dateString} ${time}`;	
+	
+}, 1000)
